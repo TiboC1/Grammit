94b2aad42e3371533c23661e575f92930147886a
TiboC1 <tibocolman@gmail.com>
2019-12-28 19:19:53 +0100

    updated database

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,14 +9,21 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{$user->username}}</h1>
-                <a href="#">Add New Post</a>
+                @can('update', $user->profile)
+                    <a href="/p/create">Add New Post</a>
+                @endcan
             </div>
+
+            @can('update', $user->profile)
+                <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @endcan
+
             <div class="d-flex">
-                <div class="pr-5"><strong>333</strong> posts</div>
+            <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>33k</strong> followers</div>
                 <div class="pr-5"><strong>33k</strong> following</div>
             </div>
-            <div class="pt-4">
+            <div class="pt-4 font-weight-bold">
                 {{$user->profile->title}}
             </div>
             <div>
@@ -29,15 +36,13 @@
         </div>
     </div>
     <div class="row pt-5">
-        <div class="col-4">
-            <img src="" alt="img1" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="" alt="img2" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="" alt="img3" class="w-100">
-        </div>
+        @foreach ($user->posts as $post)
+            <div class="col-4 pb-4">
+            <a href="/p/{{$post->id}}">
+                    <img src="/storage/{{$post->image}}" class="w-100">
+                </a>
+            </div>
+        @endforeach
     </div>
 </div>
 <div class="row pt-5">
