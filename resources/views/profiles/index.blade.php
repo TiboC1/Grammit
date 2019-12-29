cc020398c67bff3ca446173f8a8c1d04673483f1
TiboC1 <tibocolman@gmail.com>
2019-12-29 12:38:16 +0100

    fixed follow/unfollow Vue component

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,7 +10,7 @@
             <div class="d-flex justify-content-between align-items-baseline">
                 <div class="d-flex align-items-center pb-3">
                     <h1 class="h4">{{$user->username}}</h1>
-                <follow-button user-id="{{$user_id}}"></follow-button>
+                <follow-button user-id="{{$user->id}}" follows="{{$follows}}"></follow-button>
                 </div>
 
                 @can('update', $user->profile)
