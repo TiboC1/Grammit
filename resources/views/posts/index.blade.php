b44800f66fb48e4b85dc10170e4bde4d011abb69
TiboC1 <tibocolman@gmail.com>
2019-12-29 15:06:59 +0100

    added title to posts index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 <div class="container">
+    <div class="row text-center color-dark">
+        <h1 class="text-center">Recent posts from people you follow</h1>
+    </div>
+    <hr>
+
     @foreach ($posts as $post)
     <div class="row">
         <div class="col-6 offset-3 pb-3 d-flex align-items-center">
